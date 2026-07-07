be4b6cb5b5bcb83397e2a83c207d1e470d1bb06a
Sean Chaney <seanchaney@Seans-Mac-mini.local>
2026-07-07 05:49:43 -0600

    Created counter function with static local variable. Varaible retained value due to compiler putting the variable into the data segment, rather than created temporarlily on the stack.

--- stage1/week1/day2/main.c
+++ stage1/week1/day2/main.c
@@ -6,6 +6,13 @@ extern void print_from_defs_file(void);
 // Uncomment line below is to test if hidden variable from linkage_defs.c is accessible
 // extern int hidden_data_val;
 
+// Function with static local variable
+void count_calls(void) {
+    static int counter = 0;
+    counter++;
+    printf("print_calls() has been executed %d times\n", counter);
+}
+
 int main(void) {
     // Show both variables exist and work in base file
     print_from_defs_file();
@@ -15,4 +22,9 @@ int main(void) {
 
     // Uncomment code below to test error of trying to access static variable
     // printf("Read hidden_data_val directly: %d\n", hidden_data_val);
+
+    // Test static local variable of count_calls()
+    for(int i = 0; i < 5; i++) {
+        count_calls();
+    }
 }
